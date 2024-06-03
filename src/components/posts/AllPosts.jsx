5be592bb934ff15233d347763c0bde3fd366e467
Garrett Smith <GSmith6719@gmail.com>
2024-06-03 09:45:37 -0500

    view all posts

--- src/components/posts/AllPosts.jsx
+++ src/components/posts/AllPosts.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../../managers/PostManager.js";
+import "./AllPosts.css"
 
 export const AllPosts = () => {
-    return <>
-    <div>All posts go here</div></>
-}
+    const [posts, setPosts] = useState([]);
+  
+    useEffect(() => {
+      fetchPosts().then((data) => {
+        setPosts(data);
+      });
+    }, []);
+  
+    return (
+      <div className="posts">
+        <h1>All Posts</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.username}</p>
+              <p>{post.label}</p>
+              <p>{post.publication_date}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
