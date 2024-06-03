8cbabac04f6b833654cc73a2caccbe54831a21e7
Danny Hidalgo <dnnysongs@gmail.com>
2024-06-03 13:52:26 -0500

    work inprog

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState,useEffect } from "react";
+import { getAllCategories } from "../../managers/CategoryManager.js";
+
+
 
 export const Categories = () => {
+    
+    
+    const [allCategories, setAllcategories]= useState([])
+    
+    
+
+  useEffect(()=>{
+    getAllCategories().then(array=>{
+        setAllcategories(array)
+    })
+    console.log("categories are set in state")
+  })
+
+
+    
+    
+    
+    
+    
+    
     return <>
     <div>Here are some categories</div>
     
--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -12,3 +12,9 @@ export const createCategory =(newCategory)=>{
 
 
 }
+
+export const getAllCategories =()=>{
+return fetch(`http://localhost:8000/categories`).then((res) => res.json())
+
+
+}
